1254e8dd699e771cd7d278c789e509d8a8ad27dc
mustafasaeed23 <mustafasaedd5580@gmail.com>
2025-03-07 04:04:35 +0200

    added firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:parking_app/core/routes/app_router.dart';
-import 'package:parking_app/featuers/parking_app.dart';
+import 'package:parking_app/firebase_options.dart';
+import 'package:parking_app/parking_app.dart';
 
-void main() {
-  runApp(
-    ParkingApp(
-      appRouter: AppRouter(),
-    ),
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(ParkingApp(
+    appRouter: AppRouter(),
+  ));
 }
